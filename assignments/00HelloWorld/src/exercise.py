cf5cd232210759112beeb0497a9a263f12c6adbd
JuannAraiza <a01740671@itesm.mx>
2021-08-17 14:16:43 +0000

    practica de esfera terminada

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,16 @@
+import math  
+
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    print("Este programa calcula el area y el volumen de una esfera")
+
+    radio = float(input("Dame el valor del radio: "))
+
+    area = 4 * math.pi * math.pow(radio,2)
+    volumen = (4/3) * math.pi * math.pow(radio,3)
+
+    print(f"El area de la esfera es: {area}")
+    print(f"El volumen de la esfera es: {volumen}") 
 
 if __name__=='__main__':
     main()
